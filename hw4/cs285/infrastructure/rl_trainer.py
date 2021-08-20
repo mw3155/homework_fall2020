cf01a140a6a869e5744365a3d70c3d66d51cc7bd
Markus Weiss <markus.weiss@igz.com>
2021-08-20 22:59:14 +0200

    🎉 copy rl_trainer and utils from hw3 to hw4

--- hw4/cs285/infrastructure/rl_trainer.py
+++ hw4/cs285/infrastructure/rl_trainer.py
@@ -180,7 +180,7 @@ class RL_Trainer(object):
     def collect_training_trajectories(self, itr, initial_expertdata, collect_policy, num_transitions_to_sample, save_expert_data_to_disk=False):
         """
         :param itr:
-        :param load_initial_expertdata:  path to expert data pkl file
+        :param initial_expertdata:  path to expert data pkl file
         :param collect_policy:  the current policy using which we collect data
         :param num_transitions_to_sample:  the number of transitions we collect
         :return:
@@ -188,12 +188,56 @@ class RL_Trainer(object):
             envsteps_this_batch: the sum over the numbers of environment steps in paths
             train_video_paths: paths which also contain videos for visualization purposes
         """
-        # TODO: get this from Piazza
+        # Done: get this from hw1
+        # if your load_initial_expertdata is None, then you need to collect new trajectories at *every* iteration
+        if initial_expertdata is not None and itr == 0:
+            with open(initial_expertdata, "rb") as f:
+                loaded_paths = pickle.load(f)
+            print(loaded_paths[0].keys())
+            print("loaded n examples:", len(loaded_paths))
+            print("obs:", loaded_paths[0]["observation"].shape)
+            print("action:", loaded_paths[0]["action"].shape)
+            return loaded_paths, 0, None
+
+        # HW1 collect `batch_size` samples to be used for training
+        # HINT1: use sample_trajectories from utils
+        # HINT2: you want each of these collected rollouts to be of length self.params['ep_len']
+        print("\nCollecting data to be used for training...")
+        paths, envsteps_this_batch = utils.sample_trajectories(
+            self.env, 
+            collect_policy, 
+            min_timesteps_per_batch=num_transitions_to_sample,
+            max_path_length=self.params['ep_len'], 
+            render=False, 
+            render_mode=('rgb_array'))
+
+        # collect more rollouts with the same policy, to be saved as videos in tensorboard
+        # note: here, we collect MAX_NVIDEO rollouts, each of length MAX_VIDEO_LEN
+        train_video_paths = None
+        if self.log_video:
+            print('\nCollecting train rollouts to be used for saving videos...')
+            ## HW1 look in utils and implement sample_n_trajectories
+            train_video_paths = utils.sample_n_trajectories(self.env, collect_policy, MAX_NVIDEO, MAX_VIDEO_LEN, True)
 
         return paths, envsteps_this_batch, train_video_paths
 
+
     def train_agent(self):
-    # TODO: get this from Piazza
+    # Done: get this from hw2
+        train_logs = []
+        for train_step in range(self.params['num_agent_train_steps_per_iter']):
+
+            # HW1 sample some data from the data buffer
+            # HINT1: use the agent's sample function
+            # HINT2: how much data = self.params['train_batch_size']
+            ob_batch, ac_batch, re_batch, next_ob_batch, terminal_batch = self.agent.sample(self.params["train_batch_size"])
+
+            # HW1 use the sampled data to train an agent
+            # HINT: use the agent's train function
+            # HINT: keep the agent's training log for debugging
+            train_log = self.agent.train(ob_batch, ac_batch, re_batch, next_ob_batch, terminal_batch)
+            train_logs.append(train_log)
+        return train_logs
 
     ####################################
     ####################################
